87f2d696cd1c06d72dfd3681c07690ae6b43b0aa
David O'Mahony <domahony@compusult.net>
2013-07-30 23:03:29 -0230

    Changed shader to incorporate blinn... looks a little better.  Not sure why phong only wasn't really working.

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -11,24 +11,27 @@ uniform int gloss;
 uniform vec3 light;
 uniform vec3 light_color;
 
-
 smooth out vec4 color;
 
 void main()
 {
-	int g = 1;
+	int g = 1000;
 	vec3 l = normalize(VIEW * vec3(0, 5, 0));
 	//vec4 l = normalize(vec4(0, 0, 10, 0));
 	vec4 lc = vec4(1,1,1,0);
 	
+	
 	vec3 n = normalize(VIEW * normal);
 	vec3 e = normalize(eye);
+	
+	vec3 h = (e + l) / length(e+l);	
 	
 	vec3 r = (2 * (dot(n,l) * n)) - l;
 	
 	vec4 mat_color = vec4(1,1,0,0);
 	
 	gl_Position = MVP * vec4(position, 1);
-	color = (lc * mat_color) * pow(clamp(dot(e, r), 0, 1), g);
+	//color = (lc * mat_color) * pow(clamp(dot(e, r), 0, 1), g);
+	color = (lc * mat_color) * pow(clamp(dot(n, h), 0, 1), g);
 	//color = vec4(n, 1);
 }
