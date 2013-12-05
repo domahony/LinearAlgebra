6f02b17f3a1784cdabd772af73049aab1962939a
David O'Mahony <domahony@compusult.net>
2013-12-04 21:50:46 -0330

    Cleaned up the Camera object. Added doxygen style comments.

--- src/Camera.h
+++ src/Camera.h
@@ -16,98 +16,148 @@
 namespace domahony {
 namespace framework {
 
+/**
+ * @class Camera
+ * @brief The camera object.
+ *
+ * This object encapsulates the camera.
+ */
+
 class Camera {
 public:
 	Camera() :
 		m_projection(glm::perspective(45.0f, 4.0f/3.0f, 0.1f, 100.0f)),
 		m_location(glm::vec3(0,2,6)),
-		m_view(glm::lookAt(
-				m_location,
-				glm::vec3(0,0,0),
-				glm::vec3(0,1,0))),
-		m_quaternion(glm::quat_cast(m_view))
+		m_quaternion(glm::quat_cast(glm::lookAt(m_location, glm::vec3(0,0,0), glm::vec3(0,1,0))))
 	{
-
-		std::cout << "Matrix: " << glm::to_string(m_view) << std::endl;
-		std::cout << "Quaternion: "
-				<< glm::to_string(m_quaternion.x) << ", "
-				<< glm::to_string(m_quaternion.y) << ", "
-				<< glm::to_string(m_quaternion.z) << ", "
-				<< glm::to_string(m_quaternion.w)
-				<< std::endl;
-		std::cout << "Matrix2: " << glm::to_string(view()) << std::endl;
-
-		std::cout << "Location: " << glm::to_string(glm::vec4(m_location.x, m_location.y, m_location.z, 0) * glm::inverse(m_view)) << std::endl;
-
 	}
 
 	void update_perspective(const int& w, const int& h) {
 		m_projection = glm::perspective(45.f, w/static_cast<float>(h), 0.1f, 100.0f);
 	}
 
+	/**
+	 * @brief Returns the view matrix.
+	 *
+	 * This matrix converts between world space and camera space.
+	 */
 	const glm::mat4 view() const {
-		return
-				glm::mat4_cast(m_quaternion) *
-				glm::translate(glm::mat4(1), m_location * -1.f);
+		return glm::mat4_cast(m_quaternion) * glm::translate(glm::mat4(1), m_location * -1.f);
 	}
 
+	/**
+	 * @brief Returns the projection matrix.
+	 *
+	 * This matrix converts between camera coordinates to screen coordinates.
+	 */
 	const glm::mat4 projection() const {
 		return m_projection;
 	}
 
+	/**
+	 * @brief Returns the location of the camera.
+	 *
+	 * I think this location is in world space.
+	 */
 	const glm::vec3 location() const {
-		return glm::vec3(glm::vec4(m_location, 0));// * (m_view));
+		return glm::vec3(glm::vec4(m_location, 0));
 	}
 
-	glm::vec3 move(const glm::vec3& v, const float& m) const {
-		// use the rotation quaternion orientation to get the direction to move in
-		return glm::vec3( (view() * glm::vec4(v.x, v.y, v.z, 0)) * 1);
-	}
 
-	glm::vec3 move2(const glm::vec3& v, const float& m) {
-
-		glm::vec3 v2 = glm::mat3(glm::inverse(view())) * v;
-
-		glm::mat4 t = glm::translate(glm::mat4(1.f), v2);
-
-		glm::vec4 n = t * glm::vec4(m_location.x, m_location.y, m_location.z, 1.f);
-
-		m_location = glm::vec3(n);
-		return v;
-	}
-
+	/**
+	 * Move the laterally left
+	 */
 	void left() {
-		//m_location += move(glm::vec3(-.1, 0, 0), -1);
 		move2(glm::vec3(-.1, 0, 0), -1);
 	}
 
+	/**
+	 * Move the laterally right
+	 */
 	void right() {
-		//m_location += move(glm::vec3(.1, 0, 0), 1);
 		move2(glm::vec3(.1, 0, 0), 1);
 	}
 
+	/**
+	 * Move the camera up
+	 */
 	void up() {
-		//m_location += move(glm::vec3(0,.1, 0), 1);
 		move2(glm::vec3(0,.1, 0), 1);
 	}
 
+	/**
+	 * Move the camera down
+	 */
 	void down() {
-		//m_location += move(glm::vec3(0,-.1, 0), -1);
 		move2(glm::vec3(0,-.1, 0), -1);
 	}
 
+	/**
+	 * Move the camera forward
+	 */
 	void in() {
-		//m_location += move(glm::vec3(0, 0, -.1), -1);
 		move2(glm::vec3(0, 0, -.1), -1);
 	}
 
+	/**
+	 * Move the camera backward
+	 */
 	void out() {
-		//m_location += move(glm::vec3(0, 0, .1), 1);
 		move2(glm::vec3(0, 0, .1), 1);
 	}
 
 
+	/**
+	 * Turn left
+	 */
+	void yaw_left() {
+		m_quaternion = m_quaternion * spin(glm::vec3(0, 1, 0), -2.f);
+	}
 
+	/**
+	 * Turn right
+	 */
+	void yaw_right() {
+		m_quaternion = m_quaternion * spin(glm::vec3(0, 1, 0), 2.f);
+	}
+
+	/**
+	 * Tilt the camera up
+	 */
+	void pitch_up() {
+		m_quaternion = m_quaternion * spin(glm::vec3(1, 0, 0), -2.f);
+	}
+
+	/**
+	 * Tilt the camera down
+	 */
+	void pitch_down() {
+		m_quaternion = m_quaternion * spin(glm::vec3(1, 0, 0), 2.f);
+	}
+
+	/**
+	 * Roll the camera to the left
+	 */
+	void roll_left() {
+		m_quaternion = m_quaternion * spin(glm::vec3(0, 0, 1), 2.f);
+	}
+
+	/**
+	 * Roll the camera to the right
+	 */
+	void roll_right() {
+		m_quaternion = m_quaternion * spin(glm::vec3(0, 0, 1), -2.f);
+	}
+
+private:
+
+	void move2(const glm::vec3& v, const float& m) {
+
+		glm::vec3 v2 = glm::normalize(glm::mat3(glm::inverse(view())) * v);
+		glm::mat4 t = glm::translate(glm::mat4(1.f), v2);
+		glm::vec4 n = t * glm::vec4(m_location.x, m_location.y, m_location.z, 1.f);
+		m_location = glm::vec3(n);
+	}
 
 	glm::quat spin(const glm::vec3& v, const float& a) const {
 		glm::vec3 axis(glm::mat3(glm::inverse(view())) * v);
@@ -115,49 +165,8 @@ public:
 		return local;
 	}
 
-	void yaw_left() {
-		m_quaternion = m_quaternion * spin(glm::vec3(0, 1, 0), 2.f);
-	}
-
-	void yaw_right() {
-		m_quaternion = m_quaternion * spin(glm::vec3(0, 1, 0), -2.f);
-	}
-
-	void pitch_up() {
-		m_quaternion = m_quaternion * spin(glm::vec3(1, 0, 0), 2.f);
-	}
-
-	void pitch_down() {
-		m_quaternion = m_quaternion * spin(glm::vec3(1, 0, 0), -2.f);
-	}
-
-	void roll_left() {
-		m_quaternion = m_quaternion * spin(glm::vec3(0, 0, 1), -2.f);
-	}
-
-	void roll_right() {
-		m_quaternion = m_quaternion * spin(glm::vec3(0, 0, 1), 2.f);
-	}
-
-private:
-	void setView() {
-
-		//m_view = glm::lookAt(m_location, glm::vec3(0,0,0), glm::vec3(0,1,0));
-		m_quaternion = glm::quat_cast(m_view);
-
-		std::cout << "Quaternion: "
-				<< glm::to_string(m_quaternion.x) << ", "
-				<< glm::to_string(m_quaternion.y) << ", "
-				<< glm::to_string(m_quaternion.z) << ", "
-				<< glm::to_string(m_quaternion.w)
-				<< std::endl;
-		std::cout << "Matrix: " << glm::to_string(m_view) << std::endl;
-	}
-
-private:
 	glm::mat4 m_projection;
 	glm::vec3 m_location;
-	glm::mat4 m_view;
 	glm::quat m_quaternion;
 
 };
